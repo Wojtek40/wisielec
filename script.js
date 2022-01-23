c00a0945b161a906faf3627330431034cfe01ad5
WB <wzbury@wp.pl>
2022-01-23 15:05:02 +0100

    fixed bug, that displayed wrong words.

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 let nullch = "_ ";
 let wd = document.getElementById("word");
 let guessed = [];
-let clicked = [];
+
+over = false;
 
 crt = 0;
 imgs = ["s0.svg", "s1.svg", "s2.svg", "s3.svg", "s4.svg", "s5.svg", "s6.svg", "s7.svg", "s8.svg", "s9.svg", "s10.svg", "s11.svg"];
@@ -13,8 +14,8 @@ function aEL(ids, word) {
     for (let i = 0; i < ids.length; i++) {
         let lt = document.getElementById(ids[i]);
         lt.addEventListener("click", function () {
-            handleLetters(ids[i], word);
-            clicked.push(ids[i]);
+            if (over) handleLetters(ids[i], word, true);
+            else handleLetters(ids[i], word, false);
             this.removeEventListener("click", arguments.callee);
         });
     }
@@ -25,7 +26,7 @@ function main() {
     word = word.toUpperCase();
     ids = ["a", "aa", "b", "c", "cc", "d", "e", "ee", "f", "g", "h", "i", "j", "k", "l", "ll", "m", "n", "nn", "o", "oo", "p", "r", "s", "ss", "t", "u", "w", "y", "z", "zi", "zz"];
     aEL(ids, word)
-    
+
     let rpl = document.getElementById("rpl");
     rpl.addEventListener("click", reset);
     write(word);
@@ -36,10 +37,10 @@ function GetRandInt(max) {
     return Math.round(Math.random() * max)
 }
 
-function handleLetters(x, word) {
+function handleLetters(x, word, won) {
     let y = document.getElementById(x);
     if (word.search(y.innerHTML) == -1) {
-        
+
         y.style.background = "red"; wrong();
     } else {
         y.style.background = "green";
@@ -50,20 +51,28 @@ function handleLetters(x, word) {
             }
         }
     }
-    status_gry(word);
+    if (!won)
+        status_gry(word);
 }
-
+function clickIDs(ids) {
+    for (let i = 0; i < ids.length; i++) {
+        let x = document.getElementById(ids[i]);
+        x.click();
+    }
+}
 function status_gry(word) {
     if (crt >= 11) {
         wd.style.color = "red"; setTimeout(przegrana(word), 1000)
+        over = true;
     };
     if (wd.innerHTML == word) {
         wd.style.color = "green";
-        setTimeout(wygrana, 1000)
+        setTimeout(wygrana, 1000);
+        over = true;
     }
 }
 function przegrana(word) {
-    
+
     let win = document.getElementById("gover");
     win.style.display = "flex";
 
@@ -78,7 +87,7 @@ function przegrana(word) {
 
 }
 function wygrana() {
-    
+
     let win = document.getElementById("gover");
     let bl = document.getElementById("blur");
     bl.style.display = "block";
@@ -107,7 +116,7 @@ function update(toUpdate) {
             ot += toUpdate[i];
         }
     }
-    
+
     return ot;
 }
 
@@ -131,24 +140,26 @@ function clear(ids) {
 }
 
 function reset() {
+    clickIDs(ids);
+
     wd.innerHTML = "";
-    wd.style="";
-    crt = 0;
+    wd.style = "";
     imgs = ["s0.svg", "s1.svg", "s2.svg", "s3.svg", "s4.svg", "s5.svg", "s6.svg", "s7.svg", "s8.svg", "s9.svg", "s10.svg", "s11.svg"];
     let score = document.getElementById("score");
     score.src = "img/" + imgs[0];
+
+
+    guessed = [];
+
+    clear(ids);
+    over = false;
+    crt = 0;
+
     let word = generate();
     word = word.toUpperCase();
-    guessed = [];
+    aEL(ids, word);
     write(word);
-    
-    ids = ["a", "aa", "b", "c", "cc", "d", "e", "ee", "f", "g", "h", "i", "j", "k", "l", "ll", "m", "n", "nn", "o", "oo", "p", "r", "s", "ss", "t", "u", "w", "y", "z", "zi", "zz"];
-    
-    aEL(clicked, word);
-    clear(clicked);
 
-    clicked = [];
-    
     let win = document.getElementById("gover");
     win.style.display = "none";
 
